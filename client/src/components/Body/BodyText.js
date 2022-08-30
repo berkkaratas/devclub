b15e609d594ee837e084d6362954c33042d92f64
Berk Karataş <berk-karatas@protonmail.com>
2022-08-30 09:42:35 +0300

    body contents updated

---
+++ client/src/components/Body/BodyText.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function BodyText() {
+  return (
+     <div className="max-w-lg mx-auto text-center">
+     <h2 className="text-3xl font-bold sm:text-4xl">Harekete Geç</h2>
+ 
+     <p className="mt-4 text-gray-300">
+       Fethiye Belediye Fen Lisesi merkezli yazılım geliştirici yetiştiren,
+       mentorluk desteği sağlayan ve dahasını sunan FeDevClub ilgi duyan herkese
+       açıktır.
+     </p>
+   </div>
+  )
+}
--- client/src/components/Body/index.js
+++ client/src/components/Body/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import  BodyContent from './BodyContent.js'
+import  BodyGoNow from './BodyGoNow.js'
+import  BodyText from './BodyText.js'
 
 export default function Body() {
   return (
-    <div>Body</div>
+     <section className="text-white bg-gray-900">
+     <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+          <BodyText />
+          <BodyContent />
+          <BodyGoNow />
+     </div>
+   </section>
   )
 }
