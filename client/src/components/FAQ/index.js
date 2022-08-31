e066fb4ed1b8aa750aaf3f91b81a3aa1d99f0a62
Berk Karataş <berk-karatas@protonmail.com>
2022-08-31 12:45:50 +0300

    FAQ page updated

--- client/src/components/FAQ/index.js
+++ client/src/components/FAQ/index.js
@@ -1,7 +1,108 @@
 import React from 'react'
+import Footer from '../Home/Footer'
+import Header from '../Home/Header'
 
 export default function FAQ() {
   return (
-    <div>FAQ</div>
+     <div className=' bg-gray-900 ' >
+          <Header />
+     <div className="space-y-4  max-w-5xl px-4 pt-16 pb-8 mx-auto" >
+          <details className="group" open>
+          <summary
+          className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-pink-500"
+          >
+          <h5 className="font-medium text-white">
+               Lorem ipsum dolor sit amet consectetur adipisicing?
+          </h5>
+     
+          <svg
+               className="flex-shrink-0 ml-1.5 w-5 h-5 transition duration-300 group-open:-rotate-180"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 24 24"
+               stroke="currentColor"
+          >
+               <path
+               strokeLinecap="round"
+               strokeLinejoin="round"
+               strokeWidth="2"
+               d="M19 9l-7 7-7-7"
+               />
+          </svg>
+          </summary>
+     
+          <p className="px-4 mt-4 leading-relaxed text-white">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+          molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+          voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+          earum similique!
+          </p>
+          </details>
+          <details className="group">
+          <summary
+          className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-pink-500"
+          >
+          <h5 className="font-medium text-white">
+               Lorem ipsum dolor sit amet consectetur adipisicing?
+          </h5>
+     
+          <svg
+               className="flex-shrink-0 ml-1.5 w-5 h-5 transition duration-300 group-open:-rotate-180"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 24 24"
+               stroke="currentColor"
+          >
+               <path
+               strokeLinecap="round"
+               strokeLinejoin="round"
+               strokeWidth="2"
+               d="M19 9l-7 7-7-7"
+               />
+          </svg>
+          </summary>
+     
+          <p className="px-4 mt-4 leading-relaxed text-white">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+          molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+          voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+          earum similique!
+          </p>
+          </details>
+          <details className="group">
+          <summary
+          className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-pink-500"
+          >
+          <h5 className="font-medium text-white">
+               Lorem ipsum dolor sit amet consectetur adipisicing?
+          </h5>
+     
+          <svg
+               className="flex-shrink-0 ml-1.5 w-5 h-5 transition duration-300 group-open:-rotate-180"
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 24 24"
+               stroke="currentColor"
+          >
+               <path
+               strokeLinecap="round"
+               strokeLinejoin="round"
+               strokeWidth="2"
+               d="M19 9l-7 7-7-7"
+               />
+          </svg>
+          </summary>
+     
+          <p className="px-4 mt-4 leading-relaxed text-white">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+          molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+          voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+          earum similique!
+          </p>
+          </details>
+   </div>
+     <Footer></Footer>
+   </div>
+   
   )
 }
--- client/src/components/Home/Header/Navbar.js
+++ client/src/components/Home/Header/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <div>
           <nav className="hidden space-x-8 text-sm font-medium md:flex">
-            <Link className="text-white" to="/home">Anasayfa</Link>
+            <Link className="text-white" to="/">Anasayfa</Link>
             <Link className="text-white" to="/blog">Blog</Link>
             <Link className="text-white" to="/support">Destek Kanaları</Link>
             <Link className="text-white" to="/applications">Başvurular</Link>
