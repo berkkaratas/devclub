095564bb22b09f4be8c5c62e4fa536a7efb77c3a
Berk Karataş <berk-karatas@protonmail.com>
2022-09-01 13:28:17 +0300

    sidebar items updated

--- client/src/components/Blog/BlogSideMenu/index.js
+++ client/src/components/Blog/BlogSideMenu/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function BlogSideMenu() {
   return (
     <div className='relative'>
-     <div className="col-span-1 w-[280px] fixed flex   flex-col justify-between h-screen bg-gray-900 border-gray-700 border-r ">
+     <div className="col-span-1 w-[240px] fixed flex   flex-col justify-between h-screen bg-gray-900 border-gray-700 border-r ">
   <div className="px-4 py-6">
    
                      <div className="flex lg:w-0 fixed top-4 left-16 lg:flex-1">
@@ -12,8 +12,72 @@ export default function BlogSideMenu() {
                     </div>
     <nav className="fixed top-24 left-6 max-w-[280px] text-white flex-col    space-y-1">
       <a
-        href="/#"
+        href="/blog#"
         className="flex items-center px-4 py-2 text-white bg-pink-500 rounded-lg"
+      >
+        <svg class="w-5 h-5 opacity-75" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
+
+        <span className="ml-3 text-sm font-medium"> Takip Ettiklerim </span>
+      </a>
+      <hr></hr>
+
+      
+      <a
+        href="/blog#"
+        className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+      >
+        <svg class="w-5 h-5 opacity-75" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>
+
+        <span className="ml-3 text-sm font-medium"> Popüler ve Yeni </span>
+      </a>
+
+      <a
+        href="/blog#"
+        className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+      >
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 opacity-75"><path d="M9.456 4.216l-5.985 7.851c-.456.637-.583 1.402-.371 2.108l.052.155a2.384 2.384 0 002.916 1.443l2.876-.864.578 4.042a2.384 2.384 0 002.36 2.047h.234l.161-.006a2.384 2.384 0 002.2-2.041l.576-4.042 2.877.864a2.384 2.384 0 002.625-3.668L14.63 4.33a3.268 3.268 0 00-5.174-.115zm3.57.613c.16.114.298.253.411.411l5.897 7.736a.884.884 0 01-.973 1.36l-3.563-1.069a.884.884 0 00-1.129.722l-.678 4.75a.884.884 0 01-.875.759h-.234a.884.884 0 01-.875-.76l-.679-4.75a.884.884 0 00-1.128-.72l-3.563 1.068a.884.884 0 01-.973-1.36L10.56 5.24a1.767 1.767 0 012.465-.41z" fill="currentcolor" fill-rule="evenodd"></path></svg>
+
+        <span className="ml-3 text-sm font-medium"> En Çok Oylanan </span>
+      </a>
+      <a
+        href="/blog#"
+        className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+      >
+       <svg class="w-5 h-5 opacity-75" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+
+        <span className="ml-3 text-sm font-medium"> Ara </span>
+      </a>
+      <hr></hr>
+      <a
+        href="/blog#"
+        className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+      >
+        <svg class="w-5 h-5 opacity-75" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
+
+        <span className="ml-3 text-sm font-medium"> Gönderi Oluştur </span>
+      </a>
+      <hr></hr>
+      <a
+        href="/blog#"
+        className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+      >
+        <svg class="w-5 h-5 opacity-75" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path></svg>
+
+        <span className="ml-3 text-sm font-medium"> Kaydettiklerin </span>
+      </a>
+      <a
+        href="/blog#"
+        className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+      >
+        <svg class="w-5 h-5 opacity-75" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+
+        <span className="ml-3 text-sm font-medium"> Okuma Geçmişin </span>
+      </a>
+      <hr></hr>
+
+      <a
+        href="/blog#"
+        className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-100 hover:text-gray-700"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -35,140 +99,15 @@ export default function BlogSideMenu() {
           />
         </svg>
 
-        <span className="ml-3 text-sm font-medium"> General </span>
+        <span className="ml-3 text-sm font-medium"> Özelleştir</span>
       </a>
 
-      <details className="group">
-        <summary
-          className="flex items-center px-4 py-2 rounded-lg cursor-pointer hover:bg-gray-100 hover:text-gray-700"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-5 h-5 opacity-75"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-            />
-          </svg>
+     
+    </nav>
+  </div>
 
-          <span className="ml-3 text-sm font-medium"> Teams </span>
-
-          <span
-            className="ml-auto transition duration-300 shrink-0 group-open:-rotate-180"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-5 h-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </span>
-        </summary>
-
-        <nav className="mt-1.5 ml-8 flex flex-col">
-          <a
-            href="/#"
-            className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-100 hover:text-gray-700"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-5 h-5 opacity-75"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
-              />
-            </svg>
-
-            <span className="ml-3 text-sm font-medium"> Banned Users </span>
-          </a>
-
-          <a
-            href="/#"
-            className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-100 hover:text-gray-700"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-5 h-5 opacity-75"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
-
-            <span className="ml-3 text-sm font-medium"> Calendar </span>
-          </a>
-        </nav>
-      </details>
-
-      <a
-        href="/#"
-        className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-100 hover:text-gray-700"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-5 h-5 opacity-75"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
-          />
-        </svg>
-
-        <span className="ml-3 text-sm font-medium"> Billing </span>
-      </a>
-
-      <a
-        href="/#"
-        className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-100 hover:text-gray-700"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-5 h-5 opacity-75"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z"
-          />
-        </svg>
-
-        <span className="ml-3 text-sm font-medium"> Invoices </span>
-      </a>
-
-      <details className="group">
+  <div className="fixed max-w-[240px] inset-x-0 bottom-0 border-t border-gray-700 fixed left-0 bottom-0 border-gray-700 border-r">
+  <details className="group text-white">
         <summary
           className="flex items-center px-4 py-2 rounded-lg cursor-pointer hover:bg-gray-100 hover:text-gray-700"
         >
@@ -187,7 +126,7 @@ export default function BlogSideMenu() {
             />
           </svg>
 
-          <span className="ml-3 text-sm font-medium"> Account </span>
+          <span className="ml-3 text-sm font-medium"> Hesabım </span>
 
           <span
             className="ml-auto transition duration-300 shrink-0 group-open:-rotate-180"
@@ -209,7 +148,7 @@ export default function BlogSideMenu() {
 
         <nav className="mt-1.5 ml-8 flex flex-col">
           <a
-            href="/#"
+            href="/blog#"
             className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-100 hover:text-gray-700"
           >
             <svg
@@ -227,11 +166,11 @@ export default function BlogSideMenu() {
               />
             </svg>
 
-            <span className="ml-3 text-sm font-medium"> Details </span>
+            <span className="ml-3 text-sm font-medium"> Detaylar </span>
           </a>
 
           <a
-            href="/#"
+            href="/blog#"
             className="flex items-center px-4 py-2 rounded-lg hover:bg-gray-100 hover:text-gray-700"
           >
             <svg
@@ -249,7 +188,7 @@ export default function BlogSideMenu() {
               />
             </svg>
 
-            <span className="ml-3 text-sm font-medium"> Security </span>
+            <span className="ml-3 text-sm font-medium"> Güvenlik </span>
           </a>
 
           <form action="/logout">
@@ -272,27 +211,21 @@ export default function BlogSideMenu() {
                 />
               </svg>
 
-              <span className="ml-3 text-sm font-medium"> Logout </span>
+              <span className="ml-3 text-sm font-medium"> Oturumu kapat </span>
             </button>
           </form>
         </nav>
       </details>
-    </nav>
-  </div>
-
-  <div className="fixed max-w-[280px] inset-x-0 bottom-0 border-t border-gray-700 fixed left-0 bottom-0 border-gray-700 border-r">
-    <a href="/#" className="flex items-center text-white p-4 bg-gray-900 hover:bg-gray-700 shrink-0">
+    <a href="/blog#" className="flex items-center text-white p-4 bg-gray-900 hover:bg-gray-700 shrink-0">
       <img
         className="object-cover w-10 h-10 rounded-full"
         src="https://www.hyperui.dev/photos/man-4.jpeg"
         alt="Simon Lewis"
       />
 
-      <div className="ml-1.5">
+      <div className="ml-4">
         <p className="text-xs">
-          <strong className="block font-medium">Simon Lewis</strong>
-
-          <span> simonlewis@fakemail.com </span>
+          <strong className="block font-medium">@berk</strong>
         </p>
       </div>
     </a>
