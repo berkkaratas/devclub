cac4017a0cb2a14de6cfd51118c620bc311e08e7
Berk Karataş <berk-karatas@protonmail.com>
2022-09-05 12:00:20 +0300

    applications page updated

--- client/src/components/Applications/index.js
+++ client/src/components/Applications/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import PendingFeatures from '../WorkInProgress/PendingFeatures'
+import Header from '../Home/Header'
+import Footer from '../Home/Footer'
 
 export default function Applications() {
   return (
-    <div>
-      <PendingFeatures/>
+    <div class=" bg-gray-900">
+      <Header></Header>
+      <div class="mx-auto  px-64 py-32  text-white rounded-lg shadow-2xl">
+          <h2 class="ml-64 text-lg font-bold">Başvuru bulunamadı!</h2>
+
+          <p class="ml-64 mt-2 text-sm ">
+            Henüz aktif bir başvuru bulunmuyor. 
+          </p>
+
+          <div class="flex items-center mr-72 justify-end mt-8 text-xs">
+            <button type="button" class="px-4 py-2 font-medium text-gray-700 rounded bg-pink-200">Anasayfaya dön</button>
+          </div>
+        </div>
+        <Footer></Footer>
+
     </div>
   )
 }
